32cc7891075cf02fa3a227586401c9b8083ef1f9
zubin00 <test.optima123@gmail.com>
2022-11-09 23:24:56 +0500

    promise.race all allsettle any

--- server.js
+++ server.js
@@ -14,23 +14,28 @@ let sndPromise = new Promise((resolve, reject) => {
         resolve('2nd  promise')
     }, 1000)
 });
-
 let rejected_promise = new Promise((resolve, reject) => reject("Rejected Promise...."));
-const all = Promise.all([firstPromise, sndPromise, rejected_promise])
-try {
-    const all = Promise.all([firstPromise, sndPromise, rejected_promise])
-    all.then((data) => {
-        console.log('this is for promise all', data);
-    });   
-} catch (e) {
+let checkRace=Promise.race([firstPromise, sndPromise, rejected_promise]).then((data)=>{
+    console.log(data)
+}).catch((e)=>{
     console.log(e)
-}
-
-const settleAll = Promise.allSettled([firstPromise, sndPromise, rejected_promise]);
-
-settleAll.then((data) => {
-    console.log('all settle ', data)
-});
+})
 
 
+// try {
+//     const all = Promise.all([firstPromise, sndPromise, rejected_promise])
+//     all.then((data) => {
+//         console.log('this is for promise all', data);
+//     });
+    
+// } catch (e) {
+//     console.log(e)
+// }
+// const settleAll = Promise.allSettled([firstPromise, sndPromise, rejected_promise]);
+
+//     settleAll.then((data) => {
+//         console.log('all settle ', data)
+//     });
 // the only difference between promise.all and promise.allSettled is 
+
+
